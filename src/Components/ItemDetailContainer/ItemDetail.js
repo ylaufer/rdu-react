2c367d16cdf215b60767578a2ac0b3dc50947fea
ylaufer <yanina.laufer@gmail.com>
2021-09-05 03:27:51 -0300

    mejore estilos counter

--- src/Components/ItemDetailContainer/ItemDetail.js
+++ src/Components/ItemDetailContainer/ItemDetail.js
@@ -9,7 +9,7 @@ export const ItemDetail = ({category, id, bodega, nombre, desc, img, precio, var
     const [cantidad, setCantidad] = useState(1)
     const handleAdd = () => {
         addItem({
-            category, id, nombre, desc, img, precio, cantidad
+            category, id, bodega, nombre, desc, img, precio, variedad, cantidad
         })
     }
 
@@ -22,7 +22,11 @@ export const ItemDetail = ({category, id, bodega, nombre, desc, img, precio, var
                     <img src={img} alt={nombre}/>
                 </div>
                 <div className="col-4 mt-3 listado">
-                    <h4 className="bodega">{bodega}</h4>
+                    <Link to={`/category/${category}`}className="btn-volver"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+                    </svg> Volver a {category}</Link>
+                    <hr></hr>
+                    <h4 className="bodega mt-4">{bodega}</h4>
                     <h2 className="nombre">{nombre}</h2>
                     <h3 className="variedad">{variedad}</h3>
                     <hr/>
@@ -36,7 +40,7 @@ export const ItemDetail = ({category, id, bodega, nombre, desc, img, precio, var
                         agregar={handleAdd} 
                         agregado={isInCart(id)}
                     />
-                    <Link to={`/category/${category}`} className="btn-comprar mt-2">Volver a Categoría</Link>
+                    
                 </div>
             </div>
         </div>
